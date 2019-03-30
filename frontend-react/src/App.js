f078097d2fb5475a1a021a569735497bc0f01294
Alyce <alyce815@yahoo.com>
2019-03-30 15:51:28 -0700

    removed edit

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>DevConnector</code> 
+             <code>DevConnector</code> 
           </p>
           <a
             className="App-link"
